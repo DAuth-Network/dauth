324ffc5208cb9377713a4c8c560c58f99e97e83b
scott9527 <>
2023-04-12 01:09:04 +0800

    nm

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import Button from "./components/Button";
 import useSignModal from "./hooks/useSignModal";
 import useDauthModal from "./hooks/useDAuthModal";
+import useEmailModal from "./hooks/useEmailModal";
 
 function App() {
   const { Modal, showModal: showDauthModal } = useDauthModal()
   const { Modal: SignModal, showModal: showSignModal } = useSignModal()
+  const { Modal: EmailModal, showModal: showEmailModal } = useEmailModal()
   return (
     <div className="App">
       <Modal />
       <SignModal />
+      <EmailModal />
       <Button onClick={showDauthModal} className="w-64">
         show modal
       </Button>
       <Button onClick={showSignModal} className="w-64">
+        show SignModal
+      </Button>
+      <Button onClick={showEmailModal} className="w-64">
         show SignModal
       </Button>
     </div>
