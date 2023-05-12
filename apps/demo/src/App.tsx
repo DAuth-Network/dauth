32a325c39fa539417fc8de04fcee8c96ec934a68
scott0927 <126661615+scott0927@users.noreply.github.com>
2023-05-12 16:35:07 +0800

    fixed: fix react version conflict (#9)

--- apps/demo/src/App.tsx
+++ apps/demo/src/App.tsx
@@ -1,20 +1,30 @@
 import "@dauth/dauth-provider/dist/style.css";
 import "@dauth/dauth-provider/dist/tailwind.css";
 import { useSignModal, Button } from "@dauth/dauth-provider";
+import { useState } from "react";
 
 function App() {
-  const { Modal: SignModal, showModal: showSignModal } = useSignModal();
+  const { Modal: SignModal, showModal: showSignModal, closeModal: closeSignModal } = useSignModal();
+  const [token, setToken] = useState('')
   const handleSuccess = (token: string) => {
     // token is jwt token
     console.log(token)
     // TODO: handle token
+    closeSignModal()
+    setToken(token)
   }
   return (
     <div className="App">
-      <SignModal googleClientId="821654150370-regko070lj9uepk3krh09m8tpth2364h.apps.googleusercontent.com" onSuccess={handleSuccess} />
+      <SignModal onSuccess={handleSuccess} />
       <Button onClick={showSignModal} className="w-64">
         Login with Dauth
       </Button>
+      <div className="text-black w-1/2 px-10">
+        <h2 className=" text-lg py-10">
+          login success, JWT token: 
+        </h2>
+        {token}
+      </div>
     </div>
   );
 
