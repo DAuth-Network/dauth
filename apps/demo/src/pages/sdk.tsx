d57426042cd29d59614eb6f5051a49c17e482c8f
scott9527 <scott@dauth.network>
2023-07-21 13:23:40 +0800

    nm

--- apps/demo/src/pages/sdk.tsx
+++ apps/demo/src/pages/sdk.tsx
@@ -1,17 +1,14 @@
 import ReactJson from 'react-json-view'
-import { FC, useState } from "react";
-import { GoogleOAuthProvider } from "@react-oauth/google";
+import {FC, useState} from "react";
+import {GoogleOAuthProvider} from "@react-oauth/google";
 import AppleLogin from "react-apple-login";
 import GoogleLoginCom from "../components/GoogleLogin";
-import { dauth } from "../utils";
+import {dauth} from "../utils";
 import TwitterLogin from "../components/TwitterLogin";
-import {
-    IOtpConfirmReturn,
-    TSign_mode,
-    verifyProof
-} from "@dauth/core";
-import { useSearchParams } from "react-router-dom";
-import { useRequest } from "ahooks";
+import {ESignMode, IOtpConfirmReturn, TSign_mode, verifyProof} from "@dauth/core";
+import {useSearchParams} from "react-router-dom";
+import {useRequest} from "ahooks";
+import {Switch} from "../components/ui/switch.tsx";
 
 const SDK: FC = () => {
 
@@ -20,8 +17,8 @@ const SDK: FC = () => {
     const [smsOtp, setSmsOtp] = useState('')
     const [emailOtp, setEmailOtp] = useState('')
     const [requestId, setRequestId] = useState('test')
-
-    const [mode, setMode] = useState<TSign_mode>('proof')
+    const [withPlainAccount, setWithPlainAccount] = useState(false)
+    const [mode, setMode] = useState<ESignMode>(ESignMode.PROOF)
     const [result, setRes] = useState<{
         data: IOtpConfirmReturn,
         mode: TSign_mode
@@ -56,7 +53,8 @@ const SDK: FC = () => {
                 code: emailOtp,
                 request_id: requestId,
                 mode: mode,
-                id_type: 'mailto'
+                id_type: 'mailto',
+                withPlainAccount
             })
             console.log(res)
             setRes(res)
@@ -70,7 +68,8 @@ const SDK: FC = () => {
                 code: smsOtp,
                 request_id: requestId,
                 mode,
-                id_type: 'tel'
+                id_type: 'tel',
+                withPlainAccount
             })
             console.log(res)
             setRes(res)
@@ -130,11 +129,15 @@ const SDK: FC = () => {
         }
     }
     const verify = () => {
-        const proof =  result!.data
-        const isValid =  verifyProof(proof)
+        const proof = result!.data
+        const isValid = verifyProof(proof)
         if (isValid) {
 
         }
+    }
+    const onClick = (e: any) => {
+        console.log(e)
+        setWithPlainAccount(e)
     }
 
     return (
@@ -142,17 +145,26 @@ const SDK: FC = () => {
             <h2 className="text-xl pb-4">@DAuth/core example</h2>
             <div className="flex w-full">
                 <div>
-                    <div className=" bg-red-50 p-4 my-4">
+                    <div className="bg-red-50 p-4 my-4">
                         <div>
                             Global setting
                         </div>
 
                         <div>
-                            Sign mode: (jwt | proof) <input className=" py-2 border-2 w-56 rounded-sm	" value={mode} onChange={(e) => { setMode(e.target.value as TSign_mode) }} type='text' />
+                            Sign mode: (jwt | proof) <input className=" py-2 border-2 w-56 rounded-sm	" value={mode}
+                                                            onChange={(e) => {
+                                                                setMode(e.target.value as ESignMode)
+                                                            }} type='text'/>
                         </div>
                         <div>
-                            Request Id: <input className=" py-2 border-2 w-56 rounded-sm	" value={requestId} onChange={(e) => { setRequestId(e.target.value) }} type="text" />
+                            Request Id: <input className=" py-2 border-2 w-56 rounded-sm	" value={requestId}
+                                               onChange={(e) => {
+                                                   setRequestId(e.target.value)
+                                               }} type="text"/>
 
+                        </div>
+                        <div>
+                            withPlainAccount: <Switch onCheckedChange={onClick} checked={withPlainAccount}/>
                         </div>
 
                     </div>
@@ -160,15 +172,21 @@ const SDK: FC = () => {
                         <div className="text-xl">
                             Email otp example
                         </div>
-                        <div className="flex justify-between items-center"> <span className="w-16 inline-block">email:</span>
-                            <input className=" py-2 border-2 w-56 rounded-sm	" value={email} onChange={(e) => { setEmail(e.target.value) }} type="text" />
+                        <div className="flex justify-between items-center"><span
+                            className="w-16 inline-block">email:</span>
+                            <input className=" py-2 border-2 w-56 rounded-sm	" value={email} onChange={(e) => {
+                                setEmail(e.target.value)
+                            }} type="text"/>
                             <button onClick={authEmailOtp} className="w-40 ml-10">
                                 get otp
-                            </button></div>
-                        <br />
+                            </button>
+                        </div>
+                        <br/>
                         <div className="flex justify-between items-center">
                             <span className="w-16 inline-block">otp: </span>
-                            <input className=" py-2 border-2 w-56	" value={emailOtp} onChange={(e) => { setEmailOtp(e.target.value) }} type="text" />
+                            <input className=" py-2 border-2 w-56	" value={emailOtp} onChange={(e) => {
+                                setEmailOtp(e.target.value)
+                            }} type="text"/>
 
                             <button onClick={authEmailOtpConfirm} className="w-40 ml-10">
                                 confirm otp
@@ -180,15 +198,21 @@ const SDK: FC = () => {
                         <div className="text-xl">
                             SMS otp example
                         </div>
-                        <div className="flex justify-between items-center"> <span className="w-16 inline-block">Phone:</span>
-                            <input className=" py-2 border-2 w-56 rounded-sm	" value={phone} onChange={(e) => { setPhone(e.target.value) }} type="text" />
+                        <div className="flex justify-between items-center"><span
+                            className="w-16 inline-block">Phone:</span>
+                            <input className=" py-2 border-2 w-56 rounded-sm	" value={phone} onChange={(e) => {
+                                setPhone(e.target.value)
+                            }} type="text"/>
                             <button onClick={authSMSOtp} className="w-40 ml-10">
                                 get otp
-                            </button></div>
-                        <br />
+                            </button>
+                        </div>
+                        <br/>
                         <div className="flex justify-between items-center">
                             <span className="w-16 inline-block">otp: </span>
-                            <input className=" py-2 border-2 w-56	" value={smsOtp} onChange={(e) => { setSmsOtp(e.target.value) }} type="text" />
+                            <input className=" py-2 border-2 w-56	" value={smsOtp} onChange={(e) => {
+                                setSmsOtp(e.target.value)
+                            }} type="text"/>
 
                             <button onClick={authSMSOtpConfirm} className="w-40 ml-10">
                                 confirm otp
@@ -200,13 +224,14 @@ const SDK: FC = () => {
                         <div className="text-xl py-4">
                             Google oauth example
                         </div>
-                        <GoogleOAuthProvider clientId="821654150370-q5hjra4s693p61l3giv7halqf42h37o1.apps.googleusercontent.com">
+                        <GoogleOAuthProvider
+                            clientId="821654150370-q5hjra4s693p61l3giv7halqf42h37o1.apps.googleusercontent.com">
                             <GoogleLoginCom onLoginSuccess={authGoogleOAuth}></GoogleLoginCom>
                         </GoogleOAuthProvider>
                     </div>
                     <div className=" bg-red-50 p-4  mt-10">
                         <div className="text-xl py-4">
-                            Apple signin  example
+                            Apple signin example
                         </div>
                         <AppleLogin
                             callback={authAppleOAuth}
@@ -215,17 +240,18 @@ const SDK: FC = () => {
                             responseMode="query"
                             responseType="code"
 
-                            redirectURI="https://demo-api.dauth.network/" />
+                            redirectURI="https://demo-api.dauth.network/"/>
                     </div>
                     <div className=" bg-red-50 p-4  mt-10">
                         <div className="text-xl py-4">
-                            Twitter signin  example
+                            Twitter signin example
                         </div>
-                        <TwitterLogin />
+                        <TwitterLogin/>
                     </div>
                 </div>
                 <div className="p-10 w-3/5">
-                    {result && <ReactJson displayDataTypes={false} quotesOnKeys={false} name={null} collapseStringsAfterLength={128} indentWidth={2} src={result!} />}
+                    {result && <ReactJson displayDataTypes={false} quotesOnKeys={false} name={null}
+                                          collapseStringsAfterLength={128} indentWidth={2} src={result!}/>}
                     <div>
                         <button onClick={verify}>verify</button>
                     </div>
--- packages/core/src/types/index.ts
+++ packages/core/src/types/index.ts
@@ -8,7 +8,13 @@ export interface IOtpConfirmReturn {
 }
 export type TAccount_type = 'mailto' | 'tel'
 export type TID_type = 'google' | 'github' | 'apple' | 'twitter' | 'mailto' | 'tel'
-export type TSign_mode = 'jwt' | 'proof'
+export type TSign_mode = 'jwt' | 'proof' | 'jwtfb' | 'both'
+export enum ESignMode {
+    JWT = "jwt",
+    PROOF="proof",
+    JWT_FIREBASE = "jwtdb",
+    BOTH = "both"
+}
 export interface IDauthConfig {
     baseURL: string,
     clientID: string,
